b37ab64ebe2639c69370283b6973faf7c87e4765
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-02 09:27:11 -0300

    Understanding some routes/resources, HTTP methos and param types

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,50 @@ import express from 'express';
 
 const app = express();
 
-//Criação de rota
-app.get('/users', (request, response) => {
-    console.log("Listagem de usuários");
+app.use(express.json());
 
-    response.json([
-        "Hello",
-        "World",
-        "Rodrigo"
-    ]);
+const users = [
+    "Hello",
+    "World",
+    "Rodrigo"
+];
+
+//Criação de rota (recurso completo da requisição)
+//O /users é o recurso/entidade que estamos acessando
+// Usamos os query params para filtrar informações, geralmente opcionais.
+//Request body: parâmetros para criação e atualização de informações
+
+app.get('/users/:id', (request, response) => {
+    //colocamos o Number para converter o id de string pra número
+    //o request params é um parâmetro que vem da roda para identificar um recurso
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.get('/users', (request, response) => {
+    //pegando qual o query da requisição
+    const search = String(request.query.search);
+
+    //filtrando o usuário
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+    
+    console.log(data);
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
