afc23cd97a0d3198ab2028b55440cbc5c1af1b7f
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-02 14:21:29 -0300

    Aded route to register points, still missing the image upload

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -18,12 +18,55 @@ routes.get('/items', async (request, response) => {
     //deixar mais acessível ao front
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost.com:3333/uploads/${item.image}`,
         }; 
     });
 
     return response.json(serializedItems);
+});
+
+//rota para inserir ponto de coleta
+routes.post('/points', async (request,response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    //fazendo com que um insert só funcione se o outro funcionar também
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    })
+
+    const point_id = insertedIds[0];
+
+    //relacionamento com a tabela items
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        };
+    })
+    //relacionamento com a tabela items
+    await trx('point_items').insert(pointItems);
+
+    return response.json({ success: true });
 });
 
 export default routes;
