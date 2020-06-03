e7edca1392206aef5b2a6b9e1e6ceb1e2405bf98
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-03 17:25:25 -0300

    Created back to home button

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+import { FiArrowLeft } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
-    return <h1>Hello Planet</h1>
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+
+                <Link to="/">
+                    <FiArrowLeft />
+                    Voltar para home
+                </Link>
+            </header>
+        </div>
+    );
 };
 
 export default CreatePoint;
