5307bdb9fdac17db712222f2a813fdb19ede24f4
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-03 18:48:30 -0300

    Searching colect items from the API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
+//usamos interface para mostrar como estão estruturados os itens no objeto
+interface Item{
+    id: number;
+    title: string;
+    image_url: string;
+}
+
 const CreatePoint = () => {
+    //Sempre que criamos um estado array ou objeto precisamos, manualmente, informar o tipo da variável
+    //Criar estado para armazenas funções no componente
+    const [items, setItems] = useState<Item[]>([]);
+    //Vamos criar função para recarregar os itens da lista
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    
+
     return (
         <div id="page-create-point">
             <header>
@@ -93,30 +113,13 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                            
+                        ))}
                     </ul>
                 </fieldset>
 
