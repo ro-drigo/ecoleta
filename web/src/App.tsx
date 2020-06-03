237438c55260e613b000eb4060b3016764eefd5d
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-03 14:55:46 -0300

    Created home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  //Como usar ESTADO
-  const [counter, setCounter] = useState(0); //[valor do estado, função para atualizar valor do estado]
-
-  //função para cuidar do click no butão
-  function handleButtonClick(){
-    //Mudando valor do state
-    setCounter(counter + 1);
-  }
-
   return (
-        <div>
-          <Header title="Hello World"/>
-
-          <h1>{counter}</h1>
-          <button type="button" onClick={handleButtonClick}>Aumentar</button>
-        </div>
+        <Home />
   );
 }
 
