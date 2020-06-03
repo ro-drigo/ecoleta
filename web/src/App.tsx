1c101f7f1afbda6cebc732463312f101d48893d8
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-03 12:28:19 -0300

    Using the STATE concept on reactjs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
 
 function App() {
+  //Como usar ESTADO
+  const [counter, setCounter] = useState(0); //[valor do estado, função para atualizar valor do estado]
+
+  //função para cuidar do click no butão
+  function handleButtonClick(){
+    //Mudando valor do state
+    setCounter(counter + 1);
+  }
+
   return (
         <div>
           <Header title="Hello World"/>
-          <Header title="Hello"/>
 
-          <h1>Conteúdo da aplicação</h1>
+          <h1>{counter}</h1>
+          <button type="button" onClick={handleButtonClick}>Aumentar</button>
         </div>
   );
 }
