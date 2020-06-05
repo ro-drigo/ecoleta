4d34aecbef91aae375200ffbfd11e22dce9f1be7
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-05 10:39:58 -0300

    Created Home page structure

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
-
+import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native';
+//importando botão
+import { RectButton } from 'react-native-gesture-handler';
+//importar icones
+import { Feather as Icon } from '@expo/vector-icons';
 
 const Home = () => {
     return (
     
-    <View style={styles.container}>
-        <Image source={require('../../assets/logo.png')} />
-    </View>
+    <ImageBackground 
+      source={require('../../assets/home-background.png')} 
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')} />
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+        </View>
+
+        <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={() => {}}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name="arrow-right" color="#FFF" size={24} />
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>
+              Entrar
+            </Text>
+          </RectButton>
+        </View>
+    </ImageBackground>
     
     );
 };
@@ -16,6 +40,7 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
+      backgroundColor: '#F0F0F5'
     },
   
     main: {
