cea73f25079147f015845952b1c1d16bb5aa8add
ro-drigo <rodrigo.santos7202@gmail.com>
2020-06-05 16:13:41 -0300

    Searcing with inputs uf and city. Finishing the App mobile

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, ImageBackground, Image, StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 //importando botão
 import { RectButton } from 'react-native-gesture-handler';
 //importar icones
@@ -7,39 +7,67 @@ import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
+    const [uf, setUf] = useState('');
+    const [city, setCity] = useState('');
+
     const navigation = useNavigation();
 
     function handleNavigateToPoints(){
-      navigation.navigate('Points');
+      navigation.navigate('Points', {
+        uf,
+        city,
+      });
     } 
 
     return (
-    
-    <ImageBackground 
-      source={require('../../assets/home-background.png')} 
-      style={styles.container}
-      imageStyle={{ width: 274, height: 368 }}
-    >
-        <View style={styles.main}>
-          <Image source={require('../../assets/logo.png')} />
-            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-        </View>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ImageBackground 
+        source={require('../../assets/home-background.png')} 
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+          <View style={styles.main}>
+            <Image source={require('../../assets/logo.png')} />
+              <View>
+                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+              </View>
+          </View>
 
-        <View style={styles.footer}>
-          <RectButton style={styles.button} onPress={handleNavigateToPoints}>
-            <View style={styles.buttonIcon}>
-              <Text>
-                <Icon name="arrow-right" color="#FFF" size={24} />
+          <View style={styles.footer}>
+            <TextInput 
+                style={styles.input}
+                placeholder="Digite a UF"
+                maxLength={2}
+                autoCapitalize="characters"
+                autoCorrect={false}
+                value={uf}
+                onChangeText={setUf}
+              />
+
+            <TextInput 
+                style={styles.input}
+                placeholder="Digite a cidade"
+                value={city}
+                autoCorrect={false}
+                onChangeText={setCity}
+              />
+            
+
+            <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+              <View style={styles.buttonIcon}>
+                <Text>
+                  <Icon name="arrow-right" color="#FFF" size={24} />
+                </Text>
+              </View>
+              <Text style={styles.buttonText}>
+                Entrar
               </Text>
-            </View>
-            <Text style={styles.buttonText}>
-              Entrar
-            </Text>
-          </RectButton>
-        </View>
-    </ImageBackground>
-    
+            </RectButton>
+          </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+
     );
 };
 
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Constants from 'expo-constants';
 import { View, Image, StyleSheet, TouchableOpacity, Text, ScrollView, Alert } from 'react-native';
 import { Feather as Icon } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
 import * as Location from 'expo-location';
@@ -23,10 +23,16 @@ interface Point {
   
 }
 
+interface Params {
+  uf: string;
+  city: string;
+}
 
 const Points = () => {
     const navigation = useNavigation();
+    const route = useRoute();
 
+    const routeParams = route.params as Params;
 
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
     const [items, setItems] = useState<Item[]>([]);
@@ -66,14 +72,14 @@ const Points = () => {
     useEffect(() => {
       api.get('points', {
         params: {
-          city: 'Taboão da Serra',
-          uf: "SP",
-          items: [1,2]
+          city: routeParams.city,
+          uf: routeParams.uf,
+          items: selectedItems
         }
       }).then(response => {
         setPoints(response.data);
       })
-    }, []);
+    }, [selectedItems]);
 
     function handleNavigateBack(){
         navigation.goBack();
